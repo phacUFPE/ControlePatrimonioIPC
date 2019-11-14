38f33db4266a46454d062628d49991c5b93e34ba
Pedro Cruz <phac@cin.ufpe.br>
2019-11-14 02:30:19 -0300

    Added redirect if already signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to equipment_index_url
+    end
+  end
 end
