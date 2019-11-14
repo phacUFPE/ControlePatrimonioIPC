f49e337f585a2a8417ac555695af3e55d26e03aa
Pedro Cruz <phac@cin.ufpe.br>
2019-11-14 02:33:07 -0300

    Added root path to user signin with not signed

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :groups
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
