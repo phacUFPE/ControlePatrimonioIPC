5666bacf4aad4dd90e7de0415fcd7abb3d0f636f
Pedro Cruz <phac@cin.ufpe.br>
2019-11-19 23:28:42 -0300

    Rails updated from 5.1.7 to 5.2.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   devise_for :users
   resources :equipment
   resources :groups
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "equipment#index"
 
