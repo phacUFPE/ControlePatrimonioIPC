2d075f69724e2fba1ade3fb6f9362b17e6ab87dd
Pedro Cruz <phac@cin.ufpe.br>
2019-11-20 21:00:11 -0300

    Added admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create({email: "admin@ipc.com", name: "Pedro Henrique Alves Cruz", password: "@pass_ipc!admin100"})
